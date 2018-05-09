766c0d164e0bb3bdad41dd19de804f648f9e9c8d
Jakub <jochlik@email.cz>
2018-05-09 12:32:17 +0200

    kostra

---
+++ model_bank.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+class ModelBank:
+    """
+    Model banka, obsahujici vsech 7 modelu paciantu
+    """
+    MODEL_GAIN = [0.33, 0.62, 1.15, 2.10, 3.69, 6.06, 9.03]
+
+    def __init__(self):
+        self.model_bank = [Model(self.MODEL_GAIN[model_number], model_number) for model_number in range(len(self.MODEL_GAIN))]
+
+
+class Model:
+    """
+    Tato classa reprezentuje jeden model v model bance, podle obrazku 3
+    """
+    ALPHA = 0.5     # fraction of SNP recirculated
+    t_1 = 50.0      # time constant of SNP action in seconds
+    t_2 = 10.0      # time constant for flow through pulmonary circulation in seconds
+    t_3 = 30.0      # time constant for flow through systemic circulation in seconds
+    T = 50.0        # infusion delay in seconds
+
+    def __init__(self, plant_gain, model_number):
+        self.G = plant_gain
+        self.delta = self.set_delta(model_number)
+        print("startuju s parametrem ", self.G, " a deltou: ", self.delta)
+
+    @staticmethod
+    def set_delta(model_number):
+        if model_number <= 2:
+            return 0.01
+        elif model_number <= 4:
+            return 0.002
+        else:
+            return 0.0004
---
+++ snt.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+from model_bank import ModelBank
+
+
+class MMAC:
+    """
+    Multiple model adaptive control
+    """
+
+    STARTING_BLOOD_PRESSURE = 100.0
+    DESIRED_BLOOD_PRESSURE_MIDDLE_STEP = 75.0
+    DESIRED_BLOOD_PRESSURE = 50.0       # P_d -- pozadovany tlak pacienta
+    MINIMAL_BLOOD_PRESSURE = DESIRED_BLOOD_PRESSURE - 20.0      # P_l -- minimalni tlak pacienta
+
+    MAXIMAL_RECOMMENDED_DOSE = 600.0          # i_M -- 600 mikrogramu na kilo za hodinu, maximalni doporucena davka
+    DRUG_CONCETRATION = 200.0          # C_s -- mikrogram/mililitr, koncentrace latky
+
+    V = 0.05             # TODO zjistit co presne to dela
+
+    PATIENT_WEIGHT = 80.0     # W_p -- vaha pacienta v kilogramech
+
+    def __init__(self):
+        W_p = self.PATIENT_WEIGHT
+        i_M = self.MAXIMAL_RECOMMENDED_DOSE
+        C_s = self.DRUG_CONCETRATION
+        self.U_M = W_p * i_M * (1.0 / C_s)         # Maximalni povolene davkovani
+
+        self.P_l = self.MINIMAL_BLOOD_PRESSURE
+        self.P_a = self.STARTING_BLOOD_PRESSURE      # pacientuv aktualni tlak
+
+        self.model_bank = ModelBank()
+
+    def limit_actual_infusion_rate(self, u_d):
+        """ omezi infuzi do intervalu <0, U_M>, tak jak je spefikovano v rovnici 2, pro stanoveni "u"
+        :rtype: return u
+        """
+        if u_d < 0.0:
+            return 0.0
+        elif u_d <= self.U_M:
+            return u_d
+        else:
+            return self.U_M
+
+    def low_pressure_check(self, u_c):
+        """ Turn off drug infusion rate if patients blood pressure drops too low
+
+        :param u_c: infusion rate before low blood pressure check
+        :return: return 0 if patients blood pressure is too low, u_c otherwise
+        """
+        return u_c if self.P_a >= self.P_l else 0
+
+
+if __name__ == "__main__":
+    snt = MMAC()
